b39f5accb14102a7113c582d093d36775c6e63d3
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-02-01 07:21:27 -0700

    Additonal data cleaning

--- Project0/Code/P0_clean_and_analyses.R
+++ Project0/Code/P0_clean_and_analyses.R
@@ -28,11 +28,53 @@ cort.dat <- cort.dat[order(cort.dat$SubjectID, cort.dat$DAYNUMB, cort.dat$Collec
 cort.dat$samp.date <- as.Date(cort.dat$Collection.Date, format = '%m/%d/%Y')
 cort.dat$samp.date.num <- as.numeric(cort.dat$samp.date)
 
-#Create new more intuitive variable names
+#Create new, and/or more intuitive variable names
 cort.dat$samp.timepoint <- cort.dat$Collection.Sample
 cort.dat$samp.day123 <- cort.dat$DAYNUMB
 cort.dat$wake.time.per.diary <- cort.dat$Sleep.Diary.reported.wake.time
-cort.dat$wake.time.in.mins <- as.difftime(cort.dat$Sleep.Diary.reported.wake.time, format="%H:%M", units = 'mins')
+cort.dat$wake.time.diary.in.mins <- as.numeric(as.difftime(cort.dat$Sleep.Diary.reported.wake.time, format="%H:%M", units = 'mins'))
+cort.dat$time.since.waking.booklet <- cort.dat$Booklet..Sample.interval.Decimal.Time..mins.
+cort.dat$time.since.waking.electronic <- cort.dat$MEMs..Sample.interval.Decimal.Time..mins.
+
+cort.dat$cort.ugdl <- cort.dat$Cortisol..ug.dl.
+cort.dat$cort.nmol.l <- cort.dat$Cortisol..nmol.L.
+cort.dat$dhea.pgdl <- cort.dat$DHEA..pg.dl.
+cort.dat$dhea.nmol.l <- cort.dat$DHEA..nmol.L.
+
+#Manipulate booklet and electronic (MEMs) clock time so that interval time from waking can be collected
+cort.dat$book.time <- cort.dat$Booket..Clock.Time
+cort.dat$mems.time <- cort.dat$MEMs..Clock.Time
+cort.dat$book.time.mins <- as.difftime(cort.dat$Booket..Clock.Time, format="%H:%M", units = 'mins')
+cort.dat$mems.time.mins <- as.difftime(cort.dat$MEMs..Clock.Time, format="%H:%M", units = 'mins')
+  
+
+wake.times <- cort.dat[cort.dat$Collection.Sample==1,c("SubjectID", "DAYNUMB", "book.time.mins", "mems.time.mins")]
+names(wake.times)[3:4] <- c("book.wake.time.mins", "mems.wake.time.mins") 
+
+#continue: merge into cort.dat, rename d1 & d2, and compare d1 & d2 with time.since.waking.booklet time.since.waking.electronic
+cort.dat2 <- merge(cort.dat, wake.times, by=c("SubjectID", "DAYNUMB"))
+cort.dat2$d1 <- as.numeric(cort.dat2$book.time.mins - cort.dat2$book.wake.time.mins)
+cort.dat2$d2 <- as.numeric(cort.dat2$mems.time.mins - cort.dat2$mems.wake.time.mins)
+
+#do this for missing and non-missing values, and for d2 as well. Then clean up and select relevant variables and create final dataset
+cort.dat2[is.na(cort.dat2$time.since.waking.booklet) | is.na(cort.dat2$d1),]
+
+
+
+mems.wake.time <- cort.dat[cort.dat$Collection.Sample==1,c('SubjectID','mems.time.mins','DAYNUMB')]
+
+
+
+
+many to one merge for baseline time for both
+
+
+
+
+
+
+#Use reshape and be out of here. See if there are other methods
+
 
 
 
@@ -45,12 +87,11 @@ cort.dat$booklet.sleeptime.diff <- (as.difftime(cort.dat$Booket..Clock.Time, for
 
 cort.dat$samp.col.time.booklet <- cort.dat$Booket..Clock.Time
 cort.dat$samp.col.time.electronic <- cort.dat$MEMs..Clock.Time
-cort.dat$time.since.waking.booklet <- cort.dat$Booklet..Sample.interval.Decimal.Time..mins.
-cort.dat$time.since.waking.electronic <- cort.dat$MEMs..Sample.interval.Decimal.Time..mins.
+
 
 
 #Select and order variables
-cort.dat <- cort.dat[, c("SubjectID", "samp.date", "samp.date.num", "samp.timepoint", "samp.day123", "wake.time.per.diary", "wake.time.in.mins")] 
+cort.dat <- cort.dat[, c("SubjectID", "samp.date", "samp.date.num", "samp.timepoint", "samp.day123", "wake.time.per.diary", "wake.time.diary.in.mins", "time.since.waking.booklet", "time.since.waking.electronic", "cort.ugdl", "cort.nmol.l", "dhea.pgdl", "dhea.nmol.l")] 
 #By selecting these variable I only effectively dropped Booklet & MEMs sample interval variables. I don't think (at this time that they're needed further)
 
 str(cort.dat) #See variable types and values
