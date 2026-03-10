cc4495479f35c153137cf423e5bde97d7a9046d2
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-03-10 17:58:55 -0600

    Initial commit of class comments, my initial thoughts, and questions for professor

---
+++ Project2/Code/p2_thoughts.R
@@ -0,0 +1,27 @@
+BIOS 6624 (03/04/26)
+A table of hypothesis, primary and secondary, and specific aims should be in the preliminary report.
+Think about what are the hypothesis for aim 1 and 2, then go to the aims, outcomes, exploratory variables
+SHoul dhave by next week
+
+BIOS 6624 (03/09/26)
+I know I have to adjust to age and sex, these other variables may be related to each other. I need the stats to see how I can account for them in the analysis. They're not sure.
+All the cognitive measures are all continuous variables
+Similar patient populations across the different sources. She would like to know if they're different.
+
+For aim 2, interaction question. Also interested in the effect of each variable on the outcome
+
+Depending on what is found in Aim 1, 
+
+
+
+Interested in baseline as well as change. This is made known in Aim 1b.
+
+Questions for professor:
+  1)Will we be penalized/graded based on the multiplicity control we use?
+  2)
+
+
+
+
+
+
