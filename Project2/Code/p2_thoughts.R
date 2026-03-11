b9fd6854b64a08f0020ccbd02b230f4bfcba2808
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-03-10 21:19:48 -0600

    Consolidated comments and notes from writing pad, included thought on multipplicity control

--- Project2/Code/p2_thoughts.R
+++ Project2/Code/p2_thoughts.R
@@ -1,9 +1,9 @@
-BIOS 6624 (03/04/26)
+Notes on (03/04/26)
 A table of hypothesis, primary and secondary, and specific aims should be in the preliminary report.
 Think about what are the hypothesis for aim 1 and 2, then go to the aims, outcomes, exploratory variables
 SHoul dhave by next week
 
-BIOS 6624 (03/09/26)
+Notes on (03/09/26)
 I know I have to adjust to age and sex, these other variables may be related to each other. I need the stats to see how I can account for them in the analysis. They're not sure.
 All the cognitive measures are all continuous variables
 Similar patient populations across the different sources. She would like to know if they're different.
@@ -15,13 +15,47 @@ Depending on what is found in Aim 1,
 
 
 Interested in baseline as well as change. This is made known in Aim 1b.
+Note that Aim 2 builds on Aim 1, such tha tan insignificant result in Aim 1 makes one of Aim 2 analysis void
+
 
 Questions for professor:
-  1)Will we be penalized/graded based on the multiplicity control we use?
-  2)
+  1)Will we be penalized/graded based on the multiplicity control we use? What do grant reviewers like to see here? 5% overall alpha for both aims or 5% for each is okay. 
+      This also depends on the hierarchy of clinical study this is, phase 2, phase 1, practice changing, small clinical study?
+  2)Why bring in "decline in AD-signature cortical thickness over a one year period" in Aim 2 under Section C.6.b? Is this another 
+  3)Can we save space by avoiding repition in the stats sectionb, e.g. restating the hypothesis that is already stated in the Expected Outcomes sections for both aims
+  4)Can I recorganize the stats section, e.g. organize such that things common to Aim 1 and 2 are described in the same place?
+  5)Should our stats reflect the details in the section: Potential Barriers and Alternative Approaches?
+  6)
+
+Multiplicity control approach
+If the professor does not penalize multiplicity control approach, then lean towards Benhamini-Hochberg within each aim and alpha of 5% 
 
 
 
+Note to self (details not included in the preliminary analysis report due to space restriction)
+-Additonal Analyses Section
+-We will compare distibutions.
+-Secondary exploratory analyses of inflammation markers.
+-Other markers that will be measured will serve as exploratory markers.
+-Multicollinearity 
+-These variables are all measured at both timepoints previously defined.
+-Expected sample size is.
 
 
+Note
+The grant document in the OneDrive has comments and highlights
+
+
+Other notes
+check for batch effects
+flag baseline T-cell count
+Address floor and ceiling effect (as sensitivity analyses?)
+Impact of missingness
+Regression assumptions
+Is Cohens D on the standard normal scale?
+Equal variance between groups
+Comparable patient characteristics from different enrollment sites
+Control for baseline marker
+Non-linear relationship betwqeen predictor and outcome
+Variance inflation/multicollinearity
 
