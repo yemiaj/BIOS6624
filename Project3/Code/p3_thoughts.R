17a51ee917e61828e929f948e03922186673e55d
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-04-01 14:36:27 -0600

    Initial commit: Class Q & A related to this project

---
+++ Project3/Code/p3_thoughts.R
@@ -0,0 +1,25 @@
+#Questions in class, 04/01/2026
+Interested variables: age, diabetes, and BP (systolic) (want for sure to be in the model)
+Not sure if should be included: presence of CHD, BP meds, smoking status (current or not), total cholesterol, and BMI
+Interested in the baseline version of these.
+No, dont look at competing risk (you can comment on that as limitations)
+Truncate data at 10 years (not interested in time beyond that)
+Not interested in events outside stroke
+Only fixed covariates only, not time-varying covariates
+Descriptive of whether variables are changing over time, which will impact decision of whether future analysis should be time-varying
+Descriptive on whether diabetes and BP changes as time-varying, not others
+Separate models by gender, interested in factors of stroke for both sexes
+
+
+
+Exclude those who have had stroke at baseline
+
+
+
+Good to dos:
+KM plot of time to stroke by risk factors
+Work on a flow chart diagram
+
+
+
+
