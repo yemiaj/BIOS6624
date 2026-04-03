5dd0cd063ca5f3a27aeba6de6dfc1d10a53b6990
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-04-03 09:22:52 -0600

    Preliminary attempt at coding the primary endpoint of time to stroke

--- Project3/Code/p3_clean_and_analyses.R
+++ Project3/Code/p3_clean_and_analyses.R
@@ -27,7 +27,57 @@ frh.flat <- reshape(frh.dat3[, c("RANDID", "PERIOD", "STROKE", "TIMESTRK", "DEAT
                     v.names = c("STROKE", "TIMESTRK", "DEATH", "TIMEDTH"),
                     direction = "wide")
 
+frh.flat <- frh.flat[,c("RANDID", "STROKE.1", "STROKE.2","STROKE.3", "TIMESTRK.1", "TIMESTRK.2", "TIMESTRK.3",
+                        "DEATH.1", "DEATH.2", "DEATH.3", "TIMEDTH.1", "TIMEDTH.2", "TIMEDTH.3")]
 
+#Naive coding, pending detailed review of coding logic
+frh.flat$stroke.yes <- pmax(frh.flat$STROKE.1, frh.flat$STROKE.2, frh.flat$STROKE.3, na.rm = TRUE)
+frh.flat$stroke.time <- pmin(frh.flat$TIMESTRK.1, frh.flat$TIMESTRK.2, frh.flat$TIMESTRK.3, na.rm = TRUE)
+
+frh.flat$dead.yes <- pmax(frh.flat$DEATH.1, frh.flat$DEATH.2, frh.flat$DEATH.3, na.rm = TRUE)
+frh.flat$dead.time <- pmin(frh.flat$TIMEDTH.1, frh.flat$TIMEDTH.2, frh.flat$TIMEDTH.3, na.rm = TRUE)
+
+#died before stroke (may censor for stroke at the date of death) #Note this during data description
+frh.flat[frh.flat$stroke.time>frh.flat$dead.time,]
+
+#stroke and died
+View(frh.flat[(frh.flat$stroke.yes==frh.flat$dead.yes) & frh.flat$stroke.yes==1,])
+
+
+plot(survfit(Surv(stroke.time,stroke.yes)~1, frh.flat))
+
+
+
+#Coding time to stroke, calling death event too
+#This is not quite right, only select stroke events, not stroke and death
+#Then put the 10 year cap
+frh.flat$stroke2 <- ifelse(frh.flat$stroke.yes==1 | frh.flat$dead.yes==1, 1, 0)
+frh.flat$stroke.time2 <- ifelse(frh.flat$stroke2==1, pmin(frh.flat$stroke.time, frh.flat$dead.time), pmax(frh.flat$stroke.time, frh.flat$dead.time))
+
+
+
+
+
+frh.flat$stroke.time2 <- NULL
+frh.flat[frh.flat$stroke2==1]$stroke.time2 <- pmin(frh.flat$stroke.time, frh.flat$dead.time)
+frh.flat[frh.flat$stroke2==0] <- pmax(frh.flat$stroke.time, frh.flat$dead.time)
+
+
+
+
+frh.flat$stroke.yes <- c(STROKE.1, STROKE.2, STROKE.3)
+  
+  with(frh.flat, max(, na.rm = TRUE))
+
+)
+   
+
+#Missing for any of timestrk variables 
+View(frh.flat[is.na(frh.flat$TIMESTRK.1) | is.na(frh.flat$TIMESTRK.2) | is.na(frh.flat$TIMESTRK.3), ])
+View(frh.int <- frh.flat[!(is.na(frh.flat$TIMESTRK.1) | is.na(frh.flat$TIMESTRK.2) | is.na(frh.flat$TIMESTRK.3)), ])
+
+
+ 
 #Does the development of other outcomes increases chances of stroke?
 #That is, is stroke independent of other outcomes?
 #There's not much I can do about that in the context of this project, soooo...abandon complicated analysis
