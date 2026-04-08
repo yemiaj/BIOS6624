7567209f53f7bfd328a1c6ee873da306803d1882
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-04-08 17:27:31 -0600

    Added notes from class on 04/08 and my thoughts on analysis.

--- Project3/Code/p3_thoughts.R
+++ Project3/Code/p3_thoughts.R
@@ -11,7 +11,7 @@ Descriptive on whether diabetes and BP changes as time-varying, not others
 Separate models by gender, interested in factors of stroke for both sexes
 Exclude those who have had stroke at baseline
 
-#Notes from class on 04/06
+#Notes from class on 04/06 and 04/08
 The variables in both models may be the same, but if theyre different you may need to explain
 Clarify that the purpose of the model is variable selection, vs prediction
 As much n in the model as possible
@@ -19,6 +19,7 @@ Estimates for 10 year survival for ages 40, 50, 60; at 0 risk factors, diabetes,
 	baseline, diabetes, and someone with 0 risk factors, and someone with high BP (>160 systolic), someone with diabetes, high BP & diabetes, + additional profile you think is important.
 In coming up with baseline risk profile, average out the continuous.
 Variables to explore (that she is interested in): Coronary HD, BP meds, smoking status, total cholesterol, BMI
+                                                  anti-hypertensive meds, prevalent CHD, smoke, total cholesterol, BMI
 
 
 Good to dos and analysis plan (see Preliminary Analysis Plan document too):
@@ -32,5 +33,12 @@ Could use AFT
 Table of events by levels of each predictor
 Regardless of which variable selection you use, notice its pitfall in the limitations
 4 by 4 for change overtime, including NA. And the cell will be proportion of cases in the category (probability transition matrix)
+Complete case for all variables that pass the first screen (logrank test), and then final analysis will bring back those with non-missing. 
+Collapsing variables
+Univariable analysis will have KM plot, Cox PH HR, log-rank test statistic, Schoenfeld test p-value
+Other model fit statistics
 
+Median and quartile follow-up estimates
+Censored rate
+Some form of best subset model
 
