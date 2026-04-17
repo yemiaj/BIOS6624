b9da519234fcb074df3fa8e03f061f5f549aae41
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-04-17 13:26:46 -0600

    Added analyses plan details, cleaned up and added comments to codes

--- Project3/Code/p3_clean_and_analyses.R
+++ Project3/Code/p3_clean_and_analyses.R
@@ -1,47 +1,65 @@
+#######################
+
+#Codes related to the cleaning of data provided for Project 3: Framingham Heart Study: Risk Factors and Probability of Stroke by Sex
+
+######################
+
+#Import data provided on Canvas
 frh.dat <- read.csv("./Project3/DataRaw/frmgham2.csv")
 
-#Subjects with prevalent stroke at baseline
-frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"]
+#Imported data has 11627 rows and 4434 unique observations
+nrow(frh.dat); length(unique(frh.dat$RANDID))
 
-#These are 32 unique subjects
-table(duplicated(frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"]))
-length(duplicated(frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"]))
-
+#Identify subjects with prevalent stroke at baseline
+exc.stroke <- frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"]
+#There are no duplicate IDs
+table(duplicated(exc.stroke))
+#and there are 32 of them with prevalent stroke at baseline
+length(unique(exc.stroke))
 #These 32 unique subjects represents 60 observations
-dim(frh.dat[frh.dat$RANDID %in% frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"],])
+nrow(frh.dat[frh.dat$RANDID %in% exc.stroke,])
+#The breakdown is as follows, 13 participants have 1 rows, 10 have 2 rows and 9 have 3 rows
+table(table(frh.dat[frh.dat$RANDID %in% exc.stroke,1]))
 
-#Subset original data and remove these 32 with prevalent stroke
-frh.dat2 <- frh.dat[!(frh.dat$RANDID %in% frh.dat[frh.dat$PREVSTRK==1 & frh.dat$PERIOD==1, "RANDID"]), ]
 
-#Select needed and potentially useful covariates 
-frh.dat3 <- frh.dat2[,c("RANDID", "PERIOD", "SEX", 
-                        "AGE", "DIABETES", "SYSBP", 
-                        "BPMEDS", "PREVCHD", "CURSMOKE", "TOTCHOL", "BMI",
-                        "STROKE", "TIMESTRK", "DEATH", "TIMEDTH")]
+#Subset original data and remove these 32 participants with prevalent stroke
+frh.dat2 <- frh.dat[!(frh.dat$RANDID %in% exc.stroke), ]
+#There are 4402 unique subjects after excluding the 32 
+length(unique(frh.dat2$RANDID))
 
-#Assess whether TIMEDTH and TIMESTRK is indeed unique using a long format
+
+#Select needed covariates (per professor)
+frh.dat3 <- frh.dat2[,c("RANDID", "PERIOD", "SEX",                         #These are identifying variables
+                        "AGE", "DIABETES", "SYSBP",                        #These variables must be included in the model (per professor)
+                        "BPMEDS", "PREVCHD", "CURSMOKE", "TOTCHOL", "BMI", #These variables should be tested/assessed for inclusion in the model (per professor)
+                        "STROKE", "TIMESTRK", "DEATH", "TIMEDTH")]         #Variables needed to ascertain the endpoint of time to stroke
+
+
+#In a long data format, assess whether TIMEDTH and TIMESTRK is indeed unique per participant
 #Sort by subject and period first
 frh.dat3 <- frh.dat3[order(frh.dat3$RANDID, frh.dat3$PERIOD), ]
-frh.dat3$strk.diff <- c(NA, diff(frh.dat3$TIMESTRK)) #Set the first value to NA so diff() works
+frh.dat3$strk.diff <- c(NA, diff(frh.dat3$TIMESTRK)) #Set the first value to NA so that diff() works as expected
 
 #Set the first period to NA per patient so strk.diff variable can be reviewed
-#Before that, make sure that there are no duplicates by subject and period
-table(duplicated(paste0(frh.dat3$RANDID, frh.dat3$PERIOD)))
-#No duplicates by subject & period
+    #Before that, make sure that there are no duplicates by subject and period
+table(duplicated(paste0(frh.dat3$RANDID, frh.dat3$PERIOD))) #No duplicates by subject & period
+
 frh.dat3$strk.diff <- ifelse(frh.dat3$PERIOD==1, NA, frh.dat3$strk.diff)
+
 table(frh.dat3$strk.diff, exclude=NULL)
 #TIMESTRK is unique! Because the diff is 0 and the 4402 NAs represent the number of unique subjects whose diff was set to NA at period 1
-sum(table(unique(frh.dat2$RANDID))) #Unique subjects
+
 
 #Do the same for TIMEDTH
-frh.dat3$dth.diff <- c(NA, diff(frh.dat3$TIMEDTH)) #Set the first value to NA so diff() works
+frh.dat3$dth.diff <- c(NA, diff(frh.dat3$TIMEDTH))
 frh.dat3$dth.diff <- ifelse(frh.dat3$PERIOD==1, NA, frh.dat3$dth.diff)
 table(frh.dat3$dth.diff, exclude=NULL)
 #TIMEDTH is also unique! 
 
 #This uniqueness makes life and coding (below) easy!
 
-#Select needed and potentially useful covariates 
+
+#Select needed covariates
 frh.dat4 <- frh.dat2[,c("RANDID", "PERIOD", "SEX", 
                         "AGE", "DIABETES", "SYSBP", 
                         "BPMEDS", "PREVCHD", "CURSMOKE", "TOTCHOL", "BMI",
@@ -50,17 +68,18 @@ frh.dat4 <- frh.dat2[,c("RANDID", "PERIOD", "SEX",
 #Sort by subject and period
 frh.dat4 <- frh.dat4[order(frh.dat4$RANDID, frh.dat4$PERIOD), ]
 
-
-#Transform data from long to wide
-frh.flat <- reshape(frh.dat4[, c("RANDID", "PERIOD", "STROKE", "TIMESTRK", "DEATH", "TIMEDTH", "DIABETES", "SYSBP", "SEX")], 
+#Transform data from long to wide, including the 2 of the variables that we will check for time-varying changes
+frh.flat <- reshape(frh.dat4[, c("RANDID", "PERIOD", "SEX", "STROKE", "TIMESTRK", "DEATH", "TIMEDTH", "DIABETES", "SYSBP")], 
                     idvar = "RANDID", 
                     timevar = "PERIOD",
-                    v.names = c("STROKE", "TIMESTRK", "DEATH", "TIMEDTH", "DIABETES", "SYSBP", "SEX"),
+                    v.names = c("PERIOD", "SEX", "STROKE", "TIMESTRK", "DEATH", "TIMEDTH", "DIABETES", "SYSBP"),
                     direction = "wide")
 
 table(frh.flat$SEX.1, frh.flat$SEX.2, frh.flat$SEX.3) #Sex is the same across all PERIOD, so select 1
 
+#Organize variables
 frh.flat <- frh.flat[,c("RANDID", 
+                        "PERIOD.1","PERIOD.2","PERIOD.3",
                         "STROKE.1", "STROKE.2","STROKE.3", 
                         "TIMESTRK.1", "TIMESTRK.2", "TIMESTRK.3",
                         "DEATH.1", "DEATH.2", "DEATH.3", 
@@ -69,13 +88,15 @@ frh.flat <- frh.flat[,c("RANDID",
                         "SYSBP.1", "SYSBP.2", "SYSBP.3", 
                         "SEX.1")]
 
-#Coding stroke and time to stroke
+
+#Coding stroke event status and time to stroke
 
 #Stroke and death status is not missing at any period, so the logic below is okay.
   #However, it will be missing in a wide format if a patient does not have rows of data for any of the 3 PERIODs
 table(frh.dat4$STROKE, exclude=NULL)
 table(frh.dat4$DEATH, exclude=NULL)
 
+#Select maximum of STROKE and DETAH status at any period as indicator for stroke and death event respectively
 frh.flat$strk1 <- pmax(frh.flat$STROKE.1, frh.flat$STROKE.2, frh.flat$STROKE.3, na.rm = TRUE) #Select stroke event
 frh.flat$dth1 <- pmax(frh.flat$DEATH.1, frh.flat$DEATH.2, frh.flat$DEATH.3, na.rm = TRUE) #Select death event
 
@@ -88,6 +109,7 @@ frh.flat$dth.tim1 <- ifelse(frh.flat$dth1==1,
                             pmin(frh.flat$TIMEDTH.1, frh.flat$TIMEDTH.2, frh.flat$TIMEDTH.3, na.rm = TRUE), #if DEATH==1 then pick earliest TIMEDTH, 
                             pmax(frh.flat$TIMEDTH.1, frh.flat$TIMEDTH.2, frh.flat$TIMEDTH.3, na.rm = TRUE)) #otherwise pick oldest TIMEDTH
 
+#Stroke and death events
 table(frh.flat$strk1)
 table(frh.flat$dth1)
 table(frh.flat$strk1, frh.flat$dth1) #131 strokes without death; 252 death and stroke; 1273 deaths without stroke
@@ -97,44 +119,55 @@ frh.flat[frh.flat$strk.tim1>frh.flat$dth.tim1,]
 strk.7.IDs <- frh.flat[frh.flat$strk.tim1>frh.flat$dth.tim1,]$RANDID
 
 #252 cases with death and stroke events
-View(frh.flat[(frh.flat$strk1 == frh.flat$dth1) & frh.flat$strk1==1,])
+#View(frh.flat[(frh.flat$strk1 == frh.flat$dth1) & frh.flat$strk1==1,])
 #In none of these is stroke time beyond death time
-View(frh.flat[(frh.flat$strk1 == frh.flat$dth1) & frh.flat$strk1==1 & (frh.flat$strk.tim1 > frh.flat$dth.tim1),])
+#View(frh.flat[(frh.flat$strk1 == frh.flat$dth1) & frh.flat$strk1==1 & (frh.flat$strk.tim1 > frh.flat$dth.tim1),])
 
-#For the 7 cases with stroke time beyond death time, hardcode time back to death time
+#For the 7 cases with stroke time beyond death time, hardcode stroke time back to death time
 frh.flat$strk1[frh.flat$strk.tim1>frh.flat$dth.tim1] <- 0 #Censor these for stroke event (they were already censored, just making sure)
-frh.flat$strk.tim1[frh.flat$strk.tim1>frh.flat$dth.tim1] <- frh.flat[frh.flat$strk.tim1>frh.flat$dth.tim1,]$dth.tim1
+frh.flat$strk.tim1[frh.flat$strk.tim1>frh.flat$dth.tim1] <- frh.flat[frh.flat$strk.tim1>frh.flat$dth.tim1,]$dth.tim1 #Set stroke time back to death time
 frh.flat[frh.flat$RANDID %in% strk.7.IDs, ] #observe to ensure coding is as expected
 
+library(survival)
 #Number of events before and after recoding to under 10 years
-survfit(Surv(strk.tim1, strk1)~1,frh.flat)
+survfit(Surv(strk.tim1, strk1)~1,frh.flat) #383 events
 plot(survfit(Surv(strk.tim1, strk1)~1,frh.flat))
 plot(sex.srv <- survfit(Surv(strk.tim1, strk1)~SEX.1,frh.flat), col=1:2, lwd=3, mark.time = FALSE)
+sex.srv
 survdiff(Surv(strk.tim1, strk1)~SEX.1,frh.flat) #p=0.03
 cox.zph(coxph(Surv(strk.tim1, strk1)~SEX.1,frh.flat)) #PH okay based on Schoenfeld, how about log-log?
 
 
-#Now code first 10 years of follow-up
-frh.flat$stroke.time <- ifelse(frh.flat$strk.tim1 > 365.25*10, 365.25*10, frh.flat$strk.tim1) #Note the multiplier in the methods
-frh.flat$stroke.event <- ifelse(frh.flat$strk.tim1 > 365.25*10, 0, frh.flat$strk1) #Note the hardcoding in the methods too
+#Limit data to the first 10 years of follow-up
+frh.flat$stroke.time <- ifelse(frh.flat$strk.tim1 > 365.25*10, 365.25*10, frh.flat$strk.tim1)
+frh.flat$stroke.event <- ifelse(frh.flat$strk.tim1 > 365.25*10, 0, frh.flat$strk1)
 
-table(frh.flat$strk.tim1>365.25*10, frh.flat$strk1) #the number of stroke events recoded back down
+#The number of stroke events recoded back down between full follow-up and back to first 10 years
+table(frh.flat$strk.tim1>365.25*10, frh.flat$strk1)
 table(frh.flat$stroke.event,frh.flat$strk1)
 
+#Kaplan-Meier plot of stroke time and event and survival function from 0 to 10
 plot(sex.srv2 <- survfit(Surv(stroke.time, stroke.event)~SEX.1,frh.flat), col=1:2, lwd=2, lty=2, mark.time = FALSE)
-
 summary(sex.srv2, times=(0:10)*365.25)
 #Include number of events in Table 1
 
 
-#Create combined flat dataset
-flat.dat0 <- frh.flat[,c("RANDID","DIABETES.1","DIABETES.2","DIABETES.3","SYSBP.1","SYSBP.2","SYSBP.3","SEX.1","strk1","dth1","strk.tim1","dth.tim1","stroke.time","stroke.event")]
+#Create a combined flat dataset, which includes the flat dataset and other variables
+flat.dat0 <- frh.flat[,c("RANDID","PERIOD.1","PERIOD.2","PERIOD.3","DIABETES.1","DIABETES.2","DIABETES.3","SYSBP.1",
+                         "SYSBP.2","SYSBP.3","SEX.1","strk1","dth1","strk.tim1","dth.tim1","stroke.time","stroke.event")]
 
 frh.dat5 <- frh.dat2[frh.dat2$PERIOD==1, c("RANDID", "PERIOD", "SEX", 
                                            "AGE", "DIABETES", "SYSBP",
                                            "BPMEDS", "PREVCHD", "CURSMOKE", "TOTCHOL", "BMI",
                                            "STROKE", "TIMESTRK", "DEATH", "TIMEDTH")]
 flat.dat <- merge(frh.dat5, flat.dat0, by="RANDID")
+
+#Split flat.dat by gender and use this version for descriptive, preliminary analysis, schoenfeld, martingale analysis
+#Do complete case on flat.dat after selecting needed variables. Use this for variable selection
+#Go back to flat.dat and do completecase for variables in the final model. Then run final model and compare estimates for data here versus the version (if N differs)
+
+
+
 
 
 #Martingale residual analysis for age, sysbp, totchol, and bmi
@@ -148,6 +181,17 @@ flat.dat <- merge(frh.dat5, flat.dat0, by="RANDID")
 #Supp Figure 2, martingale residuals 
 #Figure 3: requested risk profiles
 #Figure 2: KM plot of males and females showing the prognostic model with 3 levels
+
+#Compare best subset containig the 3 key predictors vs backwards vs vibing (2-way intractions and polynomial terms)
+#Compare fitted vs observed for survival models
+#Eligible set, exclude first 32. Final set using complete case. Eligible set for univariable analysis. Martingale uses complete case (among continuous variables only)
+#Are there differences in missing vs others?
+
+
+#Functional form of the conts within the 3, then pairwise interaction. Then for each additional variable (ranked by LRT), test for main effect and pairwise interaction
+#Versus algorithm based
+#IF they need these profiles then the variables must be in the model!
+
 
 #Martingale residual analysis
 m.flat.dat <- flat.dat[flat.dat$SEX==1 & !is.na(flat.dat$BMI),
@@ -205,13 +249,44 @@ temp.m <- temp[temp$SEX==1, c("RANDID", "stroke.time","stroke.event", "age.cat",
 temp.f <- temp[temp$SEX==2,c("RANDID", "stroke.time","stroke.event", "age.cat", "DIABETES", "sysbp.cat", "BPMEDS",
                              "PREVCHD", "CURSMOKE", "totchol.cat", "bmi.cat")]
 
-fit1 <- survfit(Surv(temp.m[,2], temp.m[,3])~temp.m[,11], data=temp.m)
-plot(fit1, fun="cloglog", col=1:4, lwd=3, xlab="time", ylab="log(-log(S(t)))")
+fit1 <- survfit(Surv(temp.m[,2], temp.m[,3])~temp.m[,8], data=temp.m)
+fit2 <- survfit(Surv(temp.f[,2], temp.f[,3])~temp.f[,8], data=temp.f)
 
-fit2 <- survfit(Surv(temp.f[,2], temp.f[,3])~temp.f[,11], data=temp.f)
+par(mfrow=c(2,4))
+plot(fit1, lwd=3, col=1:4, ylim=c(0.9,1))
+plot(fit2, lwd=3, col=1:4, ylim=c(0.9,1))
+
+plot(fit1, fun="cloglog", col=1:4, lwd=3, xlab="time", ylab="log(-log(S(t)))")
 plot(fit2, fun="cloglog", col=1:4, lwd=3, xlab="time", ylab="log(-log(S(t)))")
 
-  
+
+
+
+mods3 <- glmulti::glmulti(
+  y = Surv(stroke.time, stroke.event)~AGE+DIABETES+SYSBP+BPMEDS+PREVCHD+CURSMOKE+TOTCHOL+BMI ,
+  fitfunction = "coxph",
+  data = m.flat.dat2,
+  intercept = TRUE,
+  level = 2,
+  method = "h",
+  crit = "aic",
+  confsetsize = 10,
+  plotty = FALSE
+)
+
+coxph(Surv(stroke.time, stroke.event)~BMI, m.flat.dat2)
+
+full_model <- coxph(Surv(stroke.time, stroke.event) ~ AGE+DIABETES+SYSBP+BPMEDS+PREVCHD+CURSMOKE+TOTCHOL+BMI, data = m.flat.dat2)
+backward_model <- stepAIC(full_model, direction = "both")
+summary(backward_model)
+
+full_model0 <- coxph(Surv(stroke.time, stroke.event) ~ AGE+DIABETES+SYSBP+BPMEDS+PREVCHD+CURSMOKE+TOTCHOL+BMI, data = f.flat.dat2)
+backward_model0 <- stepAIC(full_model0, direction = "both")
+summary(backward_model0)
+
+
+
+
 as.numeric(temp$BMI >= median(temp$TOTCHOL, na.rm=TRUE))
 
 #Only TOTCHOL and BMI have missing values, AGE and SUSBP doesn't
