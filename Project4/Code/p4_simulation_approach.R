ba94b4d50d7eb1e06c4f3a8d2cd597326565c5ff
yemiaj <opeyemi.jegede@cuanschutz.edu>
2026-04-30 16:33:58 -0600

    Made updates to methods and scenarios, and added some questions to ask during office hours.

--- Project4/Code/p4_simulation_approach.R
+++ Project4/Code/p4_simulation_approach.R
@@ -6,20 +6,18 @@ Methods
   1) backward selection based on F-test and p-value
   2) AIC
   3) BIC
-  4) LASSO, 2 choices of lambda (lambda: min and 1se)
-  5) Elastic net, 2 choices of lambda (lambda: min and 1se)
+  4) LASSO, 2 choices of lambda (lambda: min and 1se) ==> lm fit, debiased LASSO, and/or post-selection inference
+  5) Elastic net, 2 choices of lambda (lambda: min and 1se) ==> lm fit, debiased LASSO, and/or post-selection inference
 
 Scenarios
   1) N = 250, P = 20; 5 of the 20 P would be considered important with
     BETA_1:5 = 0.5/3, 1.0/3, 1.5/3, 2.0/3, 2.5/3
-    1a) Indepence among Xs
-    1b) Correlation among Xs
+    1a) Relationship among Xs: exchangeable correlation with rho c(0.00, 0.35, 0.70)
 
   2) N = 500, P = 20; 5 of the 20 P would be considered important with 
     BETA_1:5 = 0.5/3, 1.0/3, 1.5/3, 2.0/3, 2.5/3
-    2a) Indepence among Xs
-    2b) Correlation among Xs
-  
+    2a) Relationship among Xs: exchangeable correlation with rho c(0.00, 0.35, 0.70)
+
 Results of interest
   1) True positives
   2) False positives: A confusion matrix for 1) and 2)
@@ -47,3 +45,15 @@ Pick the most important statistic of interest, and use the asymptotic/simulation
 
 
 To be significant, it has to be selected AND the post-test has to be significant
+
+Reach contextual conclusion of results
+Write function to test that package is doing what it is suppossed to do for each combo of arguments
+Trusted R package, but check that is doing what is expected to do based on the arguments specified!
+Save seed for each run for dat_gen and cv.glmnet
+  
+  
+#Questions for office hours 
+1) Ask that the beta of 20-5 = 0 for the unimportant variables
+2) Bias is just for important variables, correct? The espected value for unimportant variables is 0 and bias can still be estimated
+3) Getting type I error requires simulating data under null. No? Or we are adopting a different meaning of type I error here?
+4) What if the set of 5 variables are not selected in the model? 
